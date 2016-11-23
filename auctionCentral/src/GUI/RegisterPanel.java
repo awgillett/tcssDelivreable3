3acf06b6aa33b3b95464af9d972316f3e10d38d2
Carl Seiber <carlseiber82@gmail.com>
2016-11-22 16:11:06 -0800

    GUI commit

---
+++ auctionCentral/src/GUI/RegisterPanel.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * @author RIm
+ *
+ */
+public class RegisterPanel extends JPanel {
+	
+	
+	protected void setupPanel() {
+
+		this.setLayout(null);
+		this.setSize(getPreferredSize());
+
+		JLabel userLabel = new JLabel("Register for: ");
+		userLabel.setBounds(10, 45, 80, 25);
+		this.add(userLabel);
+		
+		addContinueButton();
+		addCancelButton();
+		
+
+	}
+	
+	private void addContinueButton(){
+		JButton registerButton = new JButton("Continue");
+		registerButton.setBounds(30, 80, 80, 25);
+		this.add(registerButton);	
+		registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+            	
+            }
+        });
+	}
+	
+	private void addCancelButton(){
+		JButton registerButton = new JButton("Cancel");
+		registerButton.setBounds(160, 80, 80, 25);
+		this.add(registerButton);	
+		registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+            	MainGUI.myFrame.getContentPane().removeAll();
+            	HomeGUI homePanel = new HomeGUI(MainGUI.userList);
+            	homePanel.setupPanel();
+            	MainGUI.myFrame.add(homePanel);
+            	MainGUI.myFrame.setVisible(true);
+            	
+            }
+        });
+	}
+
+}
