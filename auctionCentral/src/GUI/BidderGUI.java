e84cf297c4f992847e1918b7ee2732b7d9ffd552
awgillett <gillett.aaron@gmail.com>
2016-11-28 09:56:15 -0800

    Added some elements to the bidder window

--- auctionCentral/src/GUI/BidderGUI.java
+++ auctionCentral/src/GUI/BidderGUI.java
@@ -12,16 +12,46 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import model.Bidder;
 import model.Calendar;
 import model.User;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JLayeredPane;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class BidderGUI extends JPanel implements ActionListener{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static final Toolkit KIT = Toolkit.getDefaultToolkit();
 	private static final Dimension SCREEN_SIZE = KIT.getScreenSize();
 	protected static JFrame myFrame = new JFrame();
 	protected static Calendar myCalendar = new Calendar();
+	static Bidder currentBidder;
+	
 	public BidderGUI() {
+		myFrame.getContentPane().setLayout(null);
+		
+//		currentBidder.setMyName("Test Bidder");
+		
+		String loggedInAs = "You are signed in as: "/* + currentBidder.getMyName()*/;
+		
+		
+		JLabel lblYouAreSigned = new JLabel(loggedInAs);
+		lblYouAreSigned.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblYouAreSigned.setHorizontalAlignment(SwingConstants.LEFT);
+		lblYouAreSigned.setBounds(22, 47, 398, 16);
+		myFrame.getContentPane().add(lblYouAreSigned);
+		
+		JLabel lblWelcomeBanner = new JLabel("Welcome to Auction Central");
+		lblWelcomeBanner.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblWelcomeBanner.setHorizontalAlignment(SwingConstants.LEFT);
+		lblWelcomeBanner.setBounds(12, 18, 286, 16);
+		myFrame.getContentPane().add(lblWelcomeBanner);
 		
 	}
 	public static void main(String[] args) {
