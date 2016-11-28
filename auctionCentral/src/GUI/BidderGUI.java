490dc9f20de34adc69da580c0451bcd06fc63d86
awgillett <gillett.aaron@gmail.com>
2016-11-27 19:48:58 -0800

    added a bidder gui class

---
+++ auctionCentral/src/GUI/BidderGUI.java
@@ -0,0 +1,25 @@
+package GUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import model.Calendar;
+import model.User;
+
+public class BidderGUI extends JPanel implements ActionListener{
+	public BidderGUI() {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
